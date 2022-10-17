1fe33d031dbf6286d583356ae32e49ff5f809754
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-17 13:25:00 -0700

    Connect GraphQL to MySQL datasource

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,24 @@ import { DataSource } from "typeorm"
 
 const main = async () => {
 
+
+    const AppDataSource = new DataSource({
+        type: "mysql",
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'rootroot',
+        database: 'ReactExchange',
+        logging: true,
+        synchronize: true,
+        entities: []
+    });
+
+    AppDataSource.initialize()
+        .then(() => console.log('Looks Like I did the databse thing right this time'))
+        .catch( err => console.log(` I think it might have broke because: ${err}`));
+
+
     const app = express();
     const port: number = 8000;
 
