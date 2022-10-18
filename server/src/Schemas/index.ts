c9832f60e5f8a52fca407a499b012a5670ec6178
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-17 18:33:09 -0700

    update Root Mutations for GraphQL

--- server/src/Schemas/index.ts
+++ server/src/Schemas/index.ts
@@ -1,19 +1,22 @@
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
 import { GET_ALL_USERS } from "./queries/userQuery";
-import { CREATE_USER } from "./mutations/userMutation";
+import { CREATE_USER, DELETE_USER, UPDATE_USER_PASSWORD } from "./mutations/userMutation";
 
 
 export const RootQuery = new GraphQLObjectType({
     name: 'RootQuery',
     fields: {
-        getAllUsers: GET_ALL_USERS
+        getAllUsers: GET_ALL_USERS,
     }
 })
 
 export const RootMutation = new GraphQLObjectType({
     name: "RootMutation",
     fields: { 
-        createUser: CREATE_USER  }
+        createUser: CREATE_USER,
+        deleteUser: DELETE_USER,
+        updatePassword: UPDATE_USER_PASSWORD
+    }
 })
 
 
