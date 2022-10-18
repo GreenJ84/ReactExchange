25315c7e0e56de473dacfea72c9603579048ff5b
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-17 18:32:56 -0700

    Created Update Password and Delete User mutations

--- server/src/Schemas/mutations/userMutation.ts
+++ server/src/Schemas/mutations/userMutation.ts
@@ -1,4 +1,5 @@
-import { GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLObjectType, GraphQLString, GraphQLID } from "graphql";
+import { Users } from "../../Entities/User";
 import { UserType } from "../typeDefs/UserType";
 
 interface argsI{
@@ -20,6 +21,43 @@ export const CREATE_USER = {
     },
     resolve(parent: any, args: argsI){
         const { firstName, lastName, username, email, password } = args;
+        Users.insert(args)
         return args;
     }
+};
+
+export const UPDATE_USER_PASSWORD = {
+    type: UserType,
+    args:{
+        username: { type: GraphQLID },
+        oldPassword: { type: GraphQLString },
+        newPassword: { type: GraphQLString }
+    },
+    async resolve(parent: any, args: any){
+        const { username, oldPassword, newPassword } = args;
+        let thisUser = await Users.findOne({where:{ username: username }});
+        if (!thisUser){
+            throw new Error("No User found with username provided");
+        }
+        const userPassword = thisUser?.password;
+        if (oldPassword === userPassword){
+            await Users.update({username: username}, {password: newPassword });
+        } else{
+            throw new Error("Password does not match!")
+        }
+        thisUser = await Users.findOne({where:{ username: username }});
+        return thisUser;
+    }
 }
+
+export const DELETE_USER = {
+    type: UserType,
+    args:{
+        id: { type: GraphQLID }
+    },
+    async resolve(parent: any, args: any){
+        const id = args.id;
+        await Users.delete(id);
+        return args;
+    }
+};
