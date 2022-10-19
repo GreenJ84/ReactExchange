43ac53714a6b50cdfa70607e935263165da6fd71
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-19 13:00:33 -0700

    Removed export from RootQuery and RootMutation

--- server/src/Schemas/index.ts
+++ server/src/Schemas/index.ts
@@ -3,14 +3,14 @@ import { GET_ALL_USERS } from "./queries/userQuery";
 import { CREATE_USER, DELETE_USER, UPDATE_USER_PASSWORD } from "./mutations/userMutation";
 
 
-export const RootQuery = new GraphQLObjectType({
+const RootQuery = new GraphQLObjectType({
     name: 'RootQuery',
     fields: {
         getAllUsers: GET_ALL_USERS,
     }
 })
 
-export const RootMutation = new GraphQLObjectType({
+const RootMutation = new GraphQLObjectType({
     name: "RootMutation",
     fields: { 
         createUser: CREATE_USER,
