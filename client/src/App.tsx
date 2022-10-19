4cff2b5e7c428f555cb50b5c6e32aca7a78119c2
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-19 14:54:43 -0700

    Added UpdatePassword component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import NewUserForm from './components/NewUserForm';
+import UpdatePassword from './components/UpdatePassword';
 import UserList from './components/UserList';
 
 
 function App() {
+  const [ updatePass, setUpdatePass] = useState(false)
 
 
   return (
       <>
-      < UserList />
-      < NewUserForm />
+      < UserList callback={() => { setUpdatePass(true)}} />
+      { updatePass ? 
+      <UpdatePassword /> :
+      < NewUserForm />}
       </>
 )};
 
